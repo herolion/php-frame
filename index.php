e12cc9df73c31c6aaa335f1f3eedbd34d7862a37
herolion <keithreay@protonmail.com>
2021-12-15 18:37:00 +0000

    Added CSS and Meta tags

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>PHP-Framework | WIP</title>
+        <meta charset="utf-8">
+        <meta name="description" content="Free Web tutorials">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <meta name="author" content="Herolion">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/styles.css">
     </head>
     <body>
         <header>
