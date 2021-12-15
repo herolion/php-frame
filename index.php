3feaeb9eaa157332804c77968a95069370732f15
herolion <keithreay@protonmail.com>
2021-12-15 18:27:21 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP-Framework | WIP</title>
+    </head>
+    <body>
+        <header>
+            Header
+        </header>
+        <main>
+            Main
+        </main>
+        <footer>
+            Footer
+        </footer>
+    </body>
+</html>
