c3540f00588c35e6aeb449daf210ee1e648489ef
herolion <keithreay@protonmail.com>
2021-12-15 20:23:46 +0000

    update

--- index.php
+++ index.php
@@ -7,17 +7,55 @@
         <meta name="keywords" content="HTML, CSS, JavaScript">
         <meta name="author" content="Herolion">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/css/styles.css">
+        <link rel="stylesheet" href="./css/styles.css">
     </head>
     <body>
-        <header>
-            Header
-        </header>
-        <main>
-            Main
-        </main>
-        <footer>
-            Footer
-        </footer>
+        <div class="row">
+            <div class="col-12 header">
+                Header
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <div class="wrapper">
+                    <div class="container">
+                        Left
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="wrapper">
+                    <div class="container">
+                        
+                        <h2>php-frame</h2>
+
+                        <p>Welcome to my Project working on building a social media with an aspect of game to it. Like a text based nations simulation game. Real profiles with the ability to create a artifical nation that they can micromanage and their friends can view and edit as the owner sees fit. This all can be viewed to profiles friends only.
+
+                        <p>I am still working on my skills with HTML, CSS, JS and PHP, so this will be a slow and painfull journey but I believe I can create something big.
+
+                        <br><h4>To-Do List</h4>
+
+                        <ul>
+                            <li>Build user system for interaction and login/registration</li>
+                            <li>Work on responsive css</li>
+                            <li>Layout of website and features</li>
+                            <li>Optimise web loading times</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="wrapper">
+                    <div class="container">
+                        Right
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 footer">
+                Footer
+            </div>
+        </div>
     </body>
 </html>
